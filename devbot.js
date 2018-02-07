5f075f9499b8266bd7e5f92d17a284d4366c85ce
heroku <mailorservant@gmail.com>
2018-02-07 07:40:44 +0100

    Macro regex

--- devbot.js
+++ devbot.js
@@ -141,7 +141,7 @@ function execDatabaseCommand(message){
   if(db==null) return
   let username=message.author.username
   let command=message.content
-  let regCreate = /\$([a-z]+)=(.+)/;
+  let regCreate = /^\$([a-z]+)=([\s\S]+)/;
   let matchCreate = regCreate.exec(command);  
   if(matchCreate!=null){
     let key=matchCreate[1]
