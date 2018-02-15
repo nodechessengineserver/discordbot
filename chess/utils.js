3298fed0225fca38bad931de4f15105d80d71e01
heroku <mailorservant@gmail.com>
2018-02-15 14:50:02 +0100

    Lichess auth

--- chess/utils.js
+++ chess/utils.js
@@ -3,7 +3,24 @@ let fetch = require("node-fetch")
 
 function checkLichess(username,code,callback){
     console.log(`checking lichess code ${username} ${code}`)
-    callback(true)
+
+    fetch(`https://lichess.org/@/${username}`).then((response)=>response.text()).
+    then((content)=>{
+        let index=content.indexOf(code)          
+
+        if(index<0){
+            console.log(`lichess auth failed for ${username}`)
+            callback(false)
+            return
+        }else{
+            console.log(`lichess auth success for ${username}`)
+            callback(true)
+            return
+        }
+    },err=>{
+        console.log(err)
+        callback(false)
+    })    
 }
 
 module.exports.checkLichess=checkLichess
