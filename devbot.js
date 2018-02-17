63cac01bf872ab4ba7b02ad6d61ce1d64a71fce6
heroku <mailorservant@gmail.com>
2018-02-17 14:41:31 +0100

    Users

--- devbot.js
+++ devbot.js
@@ -24,6 +24,33 @@ client.on("message", async message => { try {
     message.channel.send("dev ok")
   }
 
+  if(command=="users"){
+    let CHUNK=100
+    let page=parseInt(args[0])
+    if(isNaN(page)) page=1
+    page--
+    if(page<0) page=0
+    let start=page*CHUNK
+
+    let users=client.users    
+    let ids=users.keys()
+    let ida=Array.from(ids)
+
+    let content=`**Server users** ( total **${ida.length}** ) - Page **${page+1}** - Listing from **${start+1}** to **${start+CHUNK}**\n\n`
+    let ucs=[]
+    for(let i=start;i<start+CHUNK;i++){
+      if(i<ida.length){
+        let id=ida[i]
+        ucs.push(`${i+1}. **${users.get(id)["username"]}**`)
+      }
+    }
+
+    content+=ucs.join(" , ")
+    
+    console.log(content)
+    message.channel.send(content)
+  }
+
 } catch(err){
   GLOBALS.unhandledMessageError(err)
 } });
